5ad7e02ae796cd49b4cb5c954752612e49970caf
Huy Vu <vu.207ct55229@vanlanguni.vn>
2023-07-12 20:41:49 +0700

    oke

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+    DB_SERVER: 'cluster0.rlknpgs.mongodb.net',
+    DB_USER: 'darksstin',
+    DB_PASS: '09821570',
+    DB_DATABASE: 'shoppingonline',
+    EMAIL_USER: 'darksstin@gmail.com', 
+    EMAIL_PASS: 'fgpueqixkazbmdiu',
+    JWT_SECRET: 'huyvu',
+    JWT_EXPIRES: '3600000', // in milliseconds
+  };
+  module.exports = MyConstants;
